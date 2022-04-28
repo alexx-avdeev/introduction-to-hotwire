66125861e5dafa1f771d463594d988c8d476ec28
Alexey Avdeev <alexey@avdeev.io>
2022-04-27 22:49:15 -0500

    deliver streams broadcasts via activejob

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,6 +1,7 @@
 class Quote
   include Mongoid::Document
   include Mongoid::Timestamps
+  include GlobalID::Identification # need this for background streaming jobs to work
 
   include Turbo::Broadcastable
 
@@ -11,12 +12,12 @@ class Quote
   validates :text, presence: true
 
   after_create do
-    broadcast_prepend_to :quote_list
+    broadcast_prepend_later_to :quote_list
     send_flash_message({ type: 'success', text: 'Quote created.' })
   end
 
   after_update do
-    broadcast_replace_to :quote_list
+    broadcast_replace_later_to :quote_list
     send_flash_message({ type: 'warning', text: 'Quote updated.' })
   end
 
